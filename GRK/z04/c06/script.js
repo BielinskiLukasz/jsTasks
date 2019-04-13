4f06ddf2a100ca4dc3d3914b1598e5db6dad05f6
Łukasz Paweł Bieliński <l.p.bielinski@gmail.com>
2019-04-13 13:21:27 +0200

    Create console UI

---
+++ GRK/z04/c06/script.js
@@ -0,0 +1,134 @@
+var imgA;
+var imgB;
+var transformationMatrix = makeIdentityMatrix();
+showActualTransformationMatrix();
+
+function setup() {
+    createCanvas(512, 512);
+    background(255);
+    imgA = createImage(512, 512);
+    imgB = createImage(512, 512);
+    imgA.loadPixels();
+    imgB.loadPixels();
+    var d = pixelDensity();
+    for (var i = 0; i < 512 * 512 * 4 * d; i += 4) {
+        imgA.pixels[i] = 240;
+        imgA.pixels[i + 1] = 250;
+        imgA.pixels[i + 2] = 240;
+        imgA.pixels[i + 3] = 255;
+        imgB.pixels[i] = 240;
+        imgB.pixels[i + 1] = 240;
+        imgB.pixels[i + 2] = 250;
+        imgB.pixels[i + 3] = 255;
+    }
+    imgA.updatePixels();
+    imgB.updatePixels();
+}
+
+function draw() {
+    if (!keyIsDown(32)) {
+        image(imgA, 0, 0);
+        text('Image A', 10, 20);
+    } else {
+        image(imgB, 0, 0);
+        text('Image B', 10, 20);
+    }
+}
+
+function makeVector(x, y) {
+    return [x, y, 1];
+}
+
+function drawVector(img, vec) {
+    img.set(vec[0], vec[1], 200);
+    img.updatePixels();
+}
+
+function mouseDragged() {
+    drawVector(imgA, makeVector(mouseX, mouseY));
+    drawVector(imgB, transformVector(transformationMatrix, makeVector(mouseX, mouseY)));
+}
+
+function makeIdentityMatrix() {
+    return [[1, 0, 0], [0, 1, 0], [0, 0, 1]];
+}
+
+function makeTranslationMatrix(tx, ty) {
+    return [[1, 0, tx], [0, 1, ty], [0, 0, 1]];
+}
+
+function makeScaleMatrix(sx, sy) {
+    return [[sx, 0, 0], [0, sy, 0], [0, 0, 1]];
+}
+
+function makeRotationMatrix(angle) {
+    radius = angle / 180 * Math.PI;
+    return [[Math.cos(radius), -Math.sin(radius), 0], [Math.sin(radius), Math.cos(radius), 0], [0, 0, 1]];
+}
+
+function makeShearMatrix(Shx, Shy) {
+    return [[1, Shx, 0], [Shy, 1, 0], [0, 0, 1]];
+}
+
+function transformVector(matrix, vec) {
+    return [matrix[0][0] * vec[0] + matrix[0][1] * vec[1] + matrix[0][2] * vec[2],
+        matrix[1][0] * vec[0] + matrix[1][1] * vec[1] + matrix[1][2] * vec[2],
+        matrix[2][0] * vec[0] + matrix[2][1] * vec[1] + matrix[2][2] * vec[2]];
+}
+
+function multiplicationMatrix(matrix1, matrix2) {
+    return [[matrix1[0][0] * matrix2[0][0] + matrix1[0][1] * matrix2[1][0] + matrix1[0][2] * matrix2[2][0],
+        matrix1[0][0] * matrix2[0][1] + matrix1[0][1] * matrix2[1][1] + matrix1[0][2] * matrix2[2][1],
+        matrix1[0][0] * matrix2[0][2] + matrix1[0][1] * matrix2[1][2] + matrix1[0][2] * matrix2[2][2]],
+        [matrix1[1][0] * matrix2[0][0] + matrix1[1][1] * matrix2[1][0] + matrix1[1][2] * matrix2[2][0],
+            matrix1[1][0] * matrix2[0][1] + matrix1[1][1] * matrix2[1][1] + matrix1[1][2] * matrix2[2][1],
+            matrix1[1][0] * matrix2[0][2] + matrix1[1][1] * matrix2[1][2] + matrix1[1][2] * matrix2[2][2]],
+        [matrix1[2][0] * matrix2[0][0] + matrix1[2][1] * matrix2[1][0] + matrix1[2][2] * matrix2[2][0],
+            matrix1[2][0] * matrix2[0][1] + matrix1[2][1] * matrix2[1][1] + matrix1[2][2] * matrix2[2][1],
+            matrix1[2][0] * matrix2[0][2] + matrix1[2][1] * matrix2[1][2] + matrix1[2][2] * matrix2[2][2]]];
+}
+
+function grkResetTransformationMatrix() {
+    transformationMatrix = makeIdentityMatrix();
+    showActualTransformationMatrix();
+}
+
+function grkTransformMatrix(matrix) {
+    transformationMatrix = multiplicationMatrix(transformationMatrix, matrix);
+    showActualTransformationMatrix();
+}
+
+function grkTranslateTransformationMatrix(tx, ty) {
+    transformationMatrix = multiplicationMatrix(transformationMatrix, makeTranslationMatrix(tx, ty));
+    showActualTransformationMatrix();
+}
+
+function grkScaleTransformationMatrix(sx, sy) {
+    transformationMatrix = multiplicationMatrix(transformationMatrix, makeScaleMatrix(sx, sy));
+    showActualTransformationMatrix();
+}
+
+function grkRotateTransformationMatrix(angle) {
+    transformationMatrix = multiplicationMatrix(transformationMatrix, makeRotationMatrix(angle));
+    showActualTransformationMatrix();
+}
+
+function grkShearTransformationMatrix(Shx, Shy) {
+    transformationMatrix = multiplicationMatrix(transformationMatrix, makeShearMatrix(Shx, Shy));
+    showActualTransformationMatrix();
+}
+
+function grkSetTransformMatrix(matrix) {
+    transformationMatrix = matrix;
+    showActualTransformationMatrix();
+}
+
+function grkSetTransformMatrixParameter(parameter, i, j) {
+    transformationMatrix[i][j] = parameter;
+    showActualTransformationMatrix();
+}
+
+function showActualTransformationMatrix() {
+    console.log("Actual transformation matrix is: ");
+    console.log(transformationMatrix);
+}
